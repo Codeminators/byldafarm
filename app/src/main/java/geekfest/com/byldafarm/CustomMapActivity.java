5611fda6de58fb08a310ce5b9f4ac90139b7784e
pujamathur <puja.mathur95@gmail.com>
2015-03-22 04:39:47 +0530

    update swipe view

--- app/src/main/java/geekfest/com/byldafarm/CustomMapActivity.java
+++ app/src/main/java/geekfest/com/byldafarm/CustomMapActivity.java
@@ -1,42 +1,69 @@
 package geekfest.com.byldafarm;
 
-import android.os.Bundle;
+import android.graphics.Color;
+import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class CustomMapActivity extends ActionBarActivity {
 
     private GridView gridView;
+    private Button submitButton;
+    private RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_map);
         gridView = (GridView) findViewById(R.id.grid_view);
+        submitButton = (Button) findViewById(R.id.submit);
+        radioGroup = new RadioGroup(this);
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        final ArrayList<String> cropList = new ArrayList<>();
+        for(int i = 0; i < 3; i++){
+            cropList.add("Crop" + i);
+        }
+
+        for(int i = 0; i < cropList.size(); i++){
+            RadioButton button = new RadioButton(this);
+            button.setText("Button" + i);
+            button.setId(i);
+            RelativeLayout.LayoutParams layoutParams1 = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT);
+            if(i > 0) {
+                layoutParams1.addRule(RelativeLayout.RIGHT_OF, i - 1);
+            }
+            button.setLayoutParams(layoutParams1);
+            radioGroup.addView(button);
+        }
+
+        relativeLayout.addView(radioGroup);
 
         String[] blanks = new String[] {
                 "1", "2 ", " 3", " 4",
                 " 8", " 7", " 6", " 5",
                 " 9", " 10", " 11", " 12",
                 " 13", " 14", " 15", " 16",
-                "1", "2 ", " 3", " 4",
-                " 8", " 7", " 6", " 5",
-                " 9", " 10", " 11", " 12",
-                " 13", " 14", " 15", " 16",
-                "1", "2 ", " 3", " 4",
-                " 8", " 7", " 6", " 5",
-                " 9", " 10", " 11", " 12",
-                " 13", " 14", " 15", " 16",
-                "1", "2 ", " 3", " 4",
-                " 8", " 7", " 6", " 5",
-                " 9", " 10", " 11", " 12",
-                " 13", " 14", " 15", " 16"};
+                };
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, blanks);
 
         gridView.setAdapter(adapter);
@@ -47,12 +74,31 @@ public class CustomMapActivity extends ActionBarActivity {
 
                 if (event.getAction() == MotionEvent.ACTION_MOVE) {
                     gridView.requestFocusFromTouch();
-                    //     gridView.getSelectedView().setBackgroundColor(Color.parseColor("#000000"));
+//                         gridView.getFocusedChild().setBackgroundColor(Color.parseColor("#000000"));
+
                     gridView.setSelection(gridView.pointToPosition((int) event.getX(), (int) event.getY()));
+                    if(gridView.getSelectedItemPosition() != -1 && gridView.getChildAt(gridView.getSelectedItemPosition()).getTag() == null) {
+                        switch (radioGroup.getCheckedRadioButtonId()){
+                            case 0 :
+                                gridView.getChildAt(gridView.getSelectedItemPosition()).setBackgroundColor(Color.parseColor("#000000"));
+                                gridView.getChildAt(gridView.getSelectedItemPosition()).setTag(0);
+                                break;
+                            case 1 :
+                                gridView.getChildAt(gridView.getSelectedItemPosition()).setBackgroundColor(Color.parseColor("#0000FF"));
+                                gridView.getChildAt(gridView.getSelectedItemPosition()).setTag(1);
+                                break;
+                            case 2 :
+                                gridView.getChildAt(gridView.getSelectedItemPosition()).setBackgroundColor(Color.parseColor("#00FF00"));
+                                gridView.getChildAt(gridView.getSelectedItemPosition()).setTag(2);
+                                break;
+                        }
+
+                    }
+
                     return true;
                 }
                 if (event.getAction() == MotionEvent.ACTION_UP) {
-                    gridView.clearFocus();
+//                    gridView.clearFocus();
 
                     return true;
                 }
@@ -60,8 +106,36 @@ public class CustomMapActivity extends ActionBarActivity {
                 return false;
             }
         });
-    }
 
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int crop1 = 0;
+                int crop2 = 0;
+                int crop3 = 0;
+                int sum = 0;
+                for(int i = 0; i < gridView.getChildCount(); i++){
+                    if(gridView.getChildAt(i).getTag()!= null){
+                        int index = (Integer)gridView.getChildAt(i).getTag();
+                        switch (index){
+                            case 0 : crop1++;
+                                break;
+                            case 1 : crop2++;
+                                break;
+                            case 2 : crop3++;
+                                break;
+                        }
+                        sum++;
+                    }
+                }
+
+                if(sum == gridView.getChildCount()){
+                    Log.d("Crop Value", crop1 + " " + crop2 + " " + crop3);
+                }
+            }
+
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
