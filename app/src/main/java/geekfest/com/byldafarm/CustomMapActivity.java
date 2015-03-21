7e11d50775d1d880cfe7cc8c05364bb10c8114de
Raghav Apoorv <raghav.apoorv@gmail.com>
2015-03-22 01:22:19 +0530

    added the seasons

--- app/src/main/java/geekfest/com/byldafarm/CustomMapActivity.java
+++ app/src/main/java/geekfest/com/byldafarm/CustomMapActivity.java
@@ -1,7 +1,8 @@
 package geekfest.com.byldafarm;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -9,15 +10,65 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 
 public class CustomMapActivity extends ActionBarActivity {
 
     private GridView gridView;
+    private Calendar calendar;
+    private static final String WINTER = "Winter";
+    private static final String KHARIF = "Kharif";
+    private static final String RABI = "Rabi";
+    private static final String AUTUMN = "Autumn";
+    private static final String SUMMER = "Summer";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_map);
         gridView = (GridView) findViewById(R.id.grid_view);
+        calendar = Calendar.getInstance();
+
+        int month = calendar.MONTH;
+        Log.d("Raghav", ""+month);
+
+        ArrayList<String> crops = new ArrayList<>();
+
+
+        if(month == 0) {
+            crops.add(RABI);
+            crops.add(WINTER);
+        } else if(month == 1){
+            crops.add(RABI);
+            crops.add(WINTER);
+        } else if(month == 2){
+            crops.add(RABI);
+            crops.add(WINTER);
+            crops.add(SUMMER);
+        } else if(month == 3){
+            crops.add(SUMMER);
+        } else if(month == 4) {
+            crops.add(SUMMER);
+        } else if (month == 5) {
+            crops.add(SUMMER);
+        } else if (month == 6) {
+            crops.add(KHARIF);
+        } else if(month == 7){
+            crops.add(KHARIF);
+        } else if (month == 8) {
+            crops.add(KHARIF);
+        } else if (month == 9) {
+            crops.add(KHARIF);
+            crops.add(AUTUMN);
+        } else if (month == 10) {
+            crops.add(RABI);
+            crops.add(AUTUMN);
+        } else {
+            crops.add(WINTER);
+            crops.add(RABI);
+        }
+
 
         String[] blanks = new String[] {
                 "1", "2 ", " 3", " 4",
