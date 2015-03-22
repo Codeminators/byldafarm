70220a41f60ef187936cb2ba58fdce5205ce4cfe
Prempal Singh <prempal.42@gmail.com>
2015-03-22 08:33:02 +0530

    ui changes

--- app/src/main/java/geekfest/com/byldafarm/MainActivity.java
+++ app/src/main/java/geekfest/com/byldafarm/MainActivity.java
@@ -13,6 +13,11 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new MainFragment())
+                    .commit();
+        }
     }
 
 
---
+++ app/src/main/java/geekfest/com/byldafarm/MainFragment.java
@@ -0,0 +1,21 @@
+package geekfest.com.byldafarm;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by prempal on 22/3/15.
+ */
+public class MainFragment extends Fragment {
+    public MainFragment() {
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
