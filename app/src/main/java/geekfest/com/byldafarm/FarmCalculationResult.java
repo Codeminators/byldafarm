e335e1758f01b0354f4241c97ba2f054fa68e244
Raghav Apoorv <raghav.apoorv@gmail.com>
2015-04-28 16:07:06 +0530

    algorithm written

--- app/src/main/java/geekfest/com/byldafarm/FarmCalculationResult.java
+++ app/src/main/java/geekfest/com/byldafarm/FarmCalculationResult.java
@@ -4,8 +4,9 @@ package geekfest.com.byldafarm;
  * Created by raghav on 22/03/15.
  */
 public class FarmCalculationResult {
-    float totalProfit;
-    float maxAreaCrop1;
-    float maxAreaCrop2;
-    float maxAreaCrop3;
+    double totalProfit;
+    double totalCost;
+    double maxAreaCrop1;
+    double maxAreaCrop2;
+    double maxAreaCrop3;
 }
