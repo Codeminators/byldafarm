4a1df34467fa46c957bdcc6a58ffd7da0b7971fe
Prempal Singh <prempal.42@gmail.com>
2015-04-28 15:51:33 +0530

    fixed back press

--- app/src/main/java/geekfest/com/byldafarm/MainActivity.java
+++ app/src/main/java/geekfest/com/byldafarm/MainActivity.java
@@ -22,13 +22,6 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public void onBackPressed() {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, new MainFragment())
-                .commit();
-    }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
