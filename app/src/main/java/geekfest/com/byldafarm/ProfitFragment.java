9d66f446d8208dca6f0dab5aa20be95d6c0a7d26
Raghav Apoorv <raghav.apoorv@gmail.com>
2015-04-29 23:36:23 +0530

    trying to fix the pie chart, still not fixed

--- app/src/main/java/geekfest/com/byldafarm/ProfitFragment.java
+++ app/src/main/java/geekfest/com/byldafarm/ProfitFragment.java
@@ -12,7 +12,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
